9c6b1af515a971cdaffd5e4b648a3d62e70504ea
spyhimself <aymantarar7@gmail.com>
2023-04-13 03:05:48 +0000

    Added the Thymleaf Model to use Back-End rendering to View

--- src/main/java/ma/emsi/patientsmvc/web/PatientController.java
+++ src/main/java/ma/emsi/patientsmvc/web/PatientController.java
@@ -1,9 +1,14 @@
 package ma.emsi.patientsmvc.web;
 
 import lombok.AllArgsConstructor;
+import ma.emsi.patientsmvc.entities.Patient;
 import ma.emsi.patientsmvc.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -11,4 +16,11 @@ public class PatientController {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    @GetMapping(path = "/index")
+    public String Patients(Model model) {
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("patientsList", patients);
+        return "Patients";
+    }
 }
