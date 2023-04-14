555fdf15febc9dd409d5a9a3be0745a8b840d750
spyhimself <aymantarar7@gmail.com>
2023-04-14 13:19:02 +0000

    Added Pagination and The RequestParam to The Index Method

--- src/main/java/ma/emsi/patientsmvc/web/PatientController.java
+++ src/main/java/ma/emsi/patientsmvc/web/PatientController.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import ma.emsi.patientsmvc.entities.Patient;
 import ma.emsi.patientsmvc.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +21,11 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping(path = "/index")
-    public String Patients(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patientsList", patients);
+    public String Patients(Model model,
+                           @RequestParam(name = "page", defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "5") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("patientsList", pagePatients.getContent());
         return "Patients";
     }
 }
